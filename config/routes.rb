927ae6f2151521a9c218c137692189e0eb3d1127
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-08-09 14:12:32 -0400

    added fallback controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :users , only: [:index]
+
+  namespace :api do
+    resources :users , only: [:index]
+  end
   
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
