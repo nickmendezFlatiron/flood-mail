4f47fc6ef1def6bbad99d90be57c9dfbcc5679fd
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-10-10 14:22:57 -0400

    Revert "added security to redis"
    This reverts commit 1b6bdcc675ee0d583de541d1d1716d97a09407f9.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://flood-mail.onrender.com "
+    origins "https://flood-mail.onrender.com/"
 
     resource "*",
       headers: :any,
