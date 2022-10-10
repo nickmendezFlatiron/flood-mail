40bdcddc7cc4c35d0f5524ece8d00a2346e52ba1
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-10-10 12:10:58 -0400

    cors for render redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.url = 'https://flood-mail.onrender.com/cable'
+config.action_cable.allowed_request_origins = [ 'https://flood-mail.onrender.com', /http:\/\/flood-mail.onrender.com.*/ ]
 end
